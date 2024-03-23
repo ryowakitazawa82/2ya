a32bbc24b7395d4eada349d4c3f2a2ca49732b37
北沢亮和 <ryowa.kitazawa@gmail.com>
2024-03-23 20:27:16 +0900

    fix: 余計なファイル削除とカウントアップの関数定義

--- public/assets/js/common/common.js
+++ public/assets/js/common/common.js
@@ -1,15 +1,28 @@
 /*
  * 共通で使用する定数
  */
-const headerElem = document.getElementById("header");
 
-/*
- * スクロール量を保存
+/**
+ * DOMの読み込みが完了したら実行
  */
-// 初期のY座標を取得
-let nowPosition = window.scrollY;
-// headerを表示させるY座標を取得
-window.addEventListener("scroll", () => {
-  nowPosition = window.scrollY;
+document.addEventListener('DOMContentLoaded', () => {
+  dateChange();
 });
 
+/**
+ * 日付をカウントアップする処理
+ */
+const startDate = new Date(2022, 2, 18); // 月は0から始まるため、3月は2となります。
+const endDate = new Date(2024, 2, 27);
+const diffTime = Math.abs(endDate.getTime() - startDate.getTime());
+const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); // 日数の差
+const interval = 10000 / diffDays; // 10秒を日数で割ったものをインターバルとします。
+
+const dateChange = setInterval(() => {
+  startDate.setDate(startDate.getDate() + 1);
+  if (startDate >= endDate) {
+    clearInterval(dateChange);
+  }
+  document.getElementById('date').innerText =
+    `${startDate.getFullYear()}年${startDate.getMonth() + 1}月${startDate.getDate()}日`;
+}, interval);
